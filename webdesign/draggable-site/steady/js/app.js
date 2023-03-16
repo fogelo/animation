d00747022df8701128a9b3c5446d5d5928f7935a
Орлов Антон <fogelo@yandex.ru>
2023-03-16 14:06:51 +0300

    draggable-site

---
+++ webdesign/draggable-site/steady/js/app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+    setTimeout(() => {
+
+        document.body.classList.add('loaded')
+
+        Draggable.create(".gallery", {
+            bounds: "body",
+            inertia: true
+        })
+    }, 500)
+
+}
